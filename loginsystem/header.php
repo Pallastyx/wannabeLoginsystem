d8c57b7f743af34135786f4002910f86334d0a9f
Nick Karal <shikinick13@gmail.com>
2018-05-14 01:26:53 +0400

    Add files via upload

---
+++ loginsystem/header.php
@@ -0,0 +1,40 @@
+<?php 
+session_start();
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<link rel="stylesheet" type="text/css" href="main.css">
+</head>
+<body>
+	<header>
+		<nav>
+			<div class="main-wraper">
+				<ul>
+				    <li>
+				    	<a href="index.php">HOME</a>
+				    </li>
+				</ul>
+				    <div class="nav-login">
+				    	<?php
+				    	if (isset($_SESSION['u_id'])) {
+				    		echo '<form action="includes/logout.inc.php" method="POST">
+				    		<button type="submit" name="submit">LOGOUT</button>
+				    		</form>';
+				    	}else {
+				    		echo '<form action="includes/login.inc.php" method="POST">
+				    		<input type="text" name="uid" placeholder="Username/e-mail">
+				    		<input type="password" name="pwd" placeholder="password">
+				    		<button type="submit" name="submit">Login</button>
+				    		</form>
+				    		<a href="signup.php">Sign up</a>';
+				    	}
+				    	?>
+				    	
+				    	
+				    </div>
+			</div>
+		</nav>
+	</header>
---
+++ loginsystem/includes/dbh.inc.php
@@ -0,0 +1,10 @@
+<?php
+
+
+$dbServername = "localhost";
+$dbUsername = "root";
+$dbPasswordname = "";
+$dbName = "loginsystem";
+
+
+$conn = mysqli_connect($dbServername, $dbUsername, $dbPasswordname, $dbName);
